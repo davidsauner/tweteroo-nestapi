35670cf6ebee45560b0c533eb8d03cdea201b605
DavidSauner <davidsauner@gmail.com>
2023-08-21 14:04:06 -0300

    Fix: message error dto

--- src/dtos/tweet.dto.ts
+++ src/dtos/tweet.dto.ts
@@ -2,11 +2,11 @@ import { IsNotEmpty, IsString } from "class-validator"
 
 export class CreateTweetDto {
     @IsString()
-    @IsNotEmpty({ message: "todos os campos são necessários"})
+    @IsNotEmpty({ message: "All fields are required!"})
     username: string
 
     @IsString()
-    @IsNotEmpty({ message: " Todos os campos são necessários"})
+    @IsNotEmpty({ message: "All fields are required!"})
     tweet: string;
 
 }
